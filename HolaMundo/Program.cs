d6b6026963f9d8ccbb8af8b7ec2b20ec463bd5e9
Hugo Luis <hugoluis.system@gmail.com>
2021-03-18 11:29:53 -0500

    Add project files.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejemplo de declaraciones de una variable
+            int mes = 9;
+            string nombre= "Hugo Luis";
+            bool procesoTerminado = true;
+
+            // Interpolacion de cadena
+            Console.WriteLine(
+                $"Nombre: {nombre} \n" +
+                $"Mes: {mes} \n" +
+                $"Proceso: {procesoTerminado}\n");
+            Console.Read();
+        }
+    }
+}

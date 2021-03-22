3cecac9ef5f94eb32619f429bdb2a2c678b88384
Hugo Luis <hugoluis.system@gmail.com>
2021-03-22 12:49:08 -0500

    Operaciones de Relación

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -7,32 +7,33 @@ namespace HolaMundo
      class Program
     {
         static void Main(string[] args)
-
         {
             var a = 1;
             var b = 2;
             var c = 8;
             var d = 9;
+            Console.WriteLine("Operaciones de Relación");
+            Console.WriteLine("{0} == {1} es {2}", a, b, a == b);
+            Console.WriteLine("{0} == {0} es {1}", a, a == a);
 
-            Console.WriteLine("{0}+{1}={2}", a,b, a+b);
-            Console.WriteLine("{0}-{1}={2}", c, a, c - a);
-            Console.WriteLine("{0}*{1}={2}", b,c , b * c);
-            Console.WriteLine("{0}/{1}={2}", c, b, c/ b);
-            Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+            Console.WriteLine("{0} != {1} es {2}", c, d, c != d);
+            Console.WriteLine("{0} != {0} es {1}", b, b != b);
 
-            Console.WriteLine("");
-            //Incremento en 1 el valor de a
-            a++;
-            Console.WriteLine("El incremento de {0} es {1} pues x++= x+1", a - 1, a);
+            Console.WriteLine("{0} < {1} es {2}", a, b, a < b);
+            Console.WriteLine("{0} < {1} es {2}", c, a, c < a);
 
-            //Disminuyo en 1 el valor de a
-            a--;
-            Console.WriteLine("El decremento de {0} es {1} pues x--= x-1", a + 1, a);
-            Console.WriteLine("");
-            Console.WriteLine("Residuos");
-            Console.WriteLine("{0}%{1}={2}", b, a, b % a);
-            Console.WriteLine("{0}%{1}={2}", d, b, d % b);
+            Console.WriteLine("{0} > {1} es {2}", a, b, a > b);
+            Console.WriteLine("{0} > {1} es {2}", d, b, d > b);
+
+            Console.WriteLine("{0} <= {1} es {2}", c, d, c <= d);
+            Console.WriteLine("{0} <= {0} es {1}", b, b <= b);
+
+            Console.WriteLine("{0} >= {1} es {2}", c, d, c >= d);
+            Console.WriteLine("{0} >= {0} es {1}",  a, a >= a);
+
+            Console.WriteLine("{0} is int es  {1}", a, a is int);
+            Console.WriteLine("{0} is string es {1}", a, a is string);
+            Console.Read();
         }
-
     }
 }

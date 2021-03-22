a80679b0dbc97ecc1cdd46ccf0bdb7ee1554ac27
Hugo Luis <hugoluis.system@gmail.com>
2021-03-22 13:30:38 -0500

    Operadores Logicos

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -12,28 +12,31 @@ namespace HolaMundo
             var b = 2;
             var c = 8;
             var d = 9;
-            Console.WriteLine("Operaciones de Relación");
-            Console.WriteLine("{0} == {1} es {2}", a, b, a == b);
-            Console.WriteLine("{0} == {0} es {1}", a, a == a);
 
-            Console.WriteLine("{0} != {1} es {2}", c, d, c != d);
-            Console.WriteLine("{0} != {0} es {1}", b, b != b);
+            Console.WriteLine("Operaciones Lógicas");
+            var booleano = true;
+            Console.WriteLine("Operación Negación: !{0}={1}", booleano,!booleano);
 
-            Console.WriteLine("{0} < {1} es {2}", a, b, a < b);
-            Console.WriteLine("{0} < {1} es {2}", c, a, c < a);
+            Console.WriteLine("Conjunción");
+            Console.WriteLine("{0} & {0} = {1}", true, true & true);
+            Console.WriteLine("{0} & {0} = {1}", false, false & false);
+            Console.WriteLine("{0} & {1} = {2}", true, false, true & false);
+            Console.WriteLine("");
 
-            Console.WriteLine("{0} > {1} es {2}", a, b, a > b);
-            Console.WriteLine("{0} > {1} es {2}", d, b, d > b);
+            Console.WriteLine("Disyunción Exclusiva");
+            Console.WriteLine("{0} ^ {0} = {1}", true, true ^ true);
+            Console.WriteLine("{0} ^{0} = {1}", false, false ^ false);
+            Console.WriteLine("{0} ^ {1} = {2}", true, false, true ^ false);
+            Console.WriteLine("");
+            
+            Console.WriteLine("Disyunción Inclusiva");
+            Console.WriteLine("{0} | {0} = {1}", true, true | true);
+            Console.WriteLine("{0} | {1} = {2}", true, false, true | false);
+            Console.WriteLine("{0} | {0} = {1}", false, false | false);
+           
+            Console.WriteLine("");
+            Console.Read();Console.Read();
 
-            Console.WriteLine("{0} <= {1} es {2}", c, d, c <= d);
-            Console.WriteLine("{0} <= {0} es {1}", b, b <= b);
-
-            Console.WriteLine("{0} >= {1} es {2}", c, d, c >= d);
-            Console.WriteLine("{0} >= {0} es {1}",  a, a >= a);
-
-            Console.WriteLine("{0} is int es  {1}", a, a is int);
-            Console.WriteLine("{0} is string es {1}", a, a is string);
-            Console.Read();
         }
     }
 }

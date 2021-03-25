77c25e1ab145d25f81ec642864c18e4294ffc8da
Hugo Luis <hugoluis.system@gmail.com>
2021-03-24 23:54:39 -0500

    String

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -9,25 +9,34 @@ namespace HolaMundo
         static void Main(string[] args)
         {
             string nombre= "Hugo";
-            nombre = "Luis";
-            //Y la utilizamos dentro de un bloque interno
-            edad = 45;
-            var p = new Persona();
-            p.paisOrigen = "Perú";
+            string frase = "Morir vivir que sentido tiene sin el placer de luchar";
+
+
+            Console.WriteLine("*** Length ***");
+            Console.WriteLine(nombre.Length);
+            Console.WriteLine(frase.Length);
+
+            Console.WriteLine("\n*** Substring ***");
+            Console.WriteLine(nombre.Substring(2));
+            //A partir del sexto caracter queremos 6 caracteres
+            Console.WriteLine(frase.Substring(6, 6));
+
+            Console.WriteLine("\n*** Startswith *** ");
+            //frase empieza con morir lo cual debe ser verdadero.
+            Console.WriteLine(frase.StartsWith("inframundo"));
+            Console.WriteLine(frase.StartsWith("Morir"));
+
+            Console.WriteLine("\n*** enswith ***");
+            Console.WriteLine(frase.EndsWith("luchar"));
+            Console.WriteLine(frase.EndsWith("Morir"));
+
+            Console.WriteLine("\n*** Contains ***");
+            Console.WriteLine(frase.Contains("Hugo"));
+            Console.WriteLine(frase.Contains("placer"));
+
             Console.Read();
         }
 
-        //nombre = "Kyra";
-        //Definimos la variable edad dentro de un bloque externoa
-        static int edad = 21;
-        class Persona
-        {
-            public string paisOrigen;
-            private int edad2;
-            public int obtenerEdad()
-            {
-                return edad2;
-            }
-        }
+        
     }
 }

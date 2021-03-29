6f9afa6a56b1a92cc988c54b41264e2b688c9387
Hugo Luis <hugoluis.system@gmail.com>
2021-03-29 17:44:10 -0500

    Break y continue

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -9,17 +9,19 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            List<string> nombres = new List<string>();
+            List<string> nombres = new List<string>() { "Hugo", "Kyra", "Dayron" };
 
-            for (int i = 0 ; i < 10; i++)
+            //for (int i = 0; i < nombres.Count; i++)
+            //{
+            //    Console.WriteLine(nombres[i].ToUpper());
+            //}
+
+            int j = 0;
+            while( j< nombres.Count)
             {
-                if(i == 5)
-                {
-                    continue;
-                }
-                Console.WriteLine(i);
+                Console.WriteLine(nombres[j].ToUpper());
+                j++;
             }
-
               Console.Read();                     
         }     
     }
